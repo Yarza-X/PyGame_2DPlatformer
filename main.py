dbb7c13a9c02e88788ad8c81ca4c5fe1051fffe4
Yarza <zabolotskiyyarik@gmail.com>
2023-08-17 10:43:13 +0300

    Added event quit

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 import sys, pygame
+
+WIDTH = 400
+HEIGHT = 300
+BACKGROUND = "#6292d1"
+
+
+def game():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+
+    while True:
+        for evant in pygame.event.get():
+            if evant.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.fill(BACKGROUND)
+        pygame.display.flip()
+
+        clock.tick(60)  # FPS
+
+
+if __name__ == '__main__':
+    game()
